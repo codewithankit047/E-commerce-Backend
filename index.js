55db2d60d8baf59148ce3c4af2ea4ef2560a6b71
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-14 23:14:13 +0530

    login api

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
+});
+app.post("/login", async (req, resp) => {
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No user Found" });
+    }
+  } else {
+    resp.send({ result: "enter valid details" });
+  }
 });
 
 app.listen(5000);
