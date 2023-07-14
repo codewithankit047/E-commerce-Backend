96492e298c0c2134a60bd310e7b1c6803e4de27f
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-14 14:54:09 +0530

    connection to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-app.get("/", (req, resp) => {
-  resp.send("app is working");
-});
+
+const connectDB = async () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/shopNip", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+  const usersSchema = new mongoose.Schema(
+    {}
+    );
+  const user = mongoose.model("user", usersSchema);
+  const data = await user.find();
+  console.log("data print", data);
+};
+connectDB();
+// app.get("/", (req, resp) => {
+//   resp.send("app is working");
+// });
+
 app.listen(5000);
