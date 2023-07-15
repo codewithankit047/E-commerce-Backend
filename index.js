4002f4df171de10a353547491b7dd128f1083d04
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-15 21:17:16 +0530

    searchapi

--- index.js
+++ index.js
@@ -47,5 +47,42 @@ app.delete("/product/:id", async (req, resp) => {
   const result = await Product.deleteOne({ _id: req.params.id });
   resp.send(result);
 });
+app.get("/product/:id", async (req, resp) => {
+  const result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    resp.send(result);
+  } else {
+    resp.send("No data");
+  }
+});
+app.put("/product/:id", async (req, resp) => {
+  const result = await Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  );
+  if (result) {
+    resp.send(result);
+  } else {
+    resp.send("No data");
+  }
+});
+app.get("/search/:key", async (req, resp) => {
+  const result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { price: { $regex: req.params.key } },
+      { modal: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+    ],
+  });
+  if (result) {
+    resp.send(result);
+  } else {
+    resp.send("No data");
+  }
+});
 
 app.listen(5000);
